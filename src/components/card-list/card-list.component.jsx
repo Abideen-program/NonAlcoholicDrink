18cde9802bb377bf58ac452928b3b7e4061d98e7
Abideen Olafimihan <olafimihana@gmail.com>
2022-06-24 10:45:46 +0100

    changing card-list to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,29 @@
-import { Component } from "react";
-import Card from '../card/card.component'
+// import { Component } from "react";
+import Card from "../card/card.component";
 import "./card-list.component.css";
 
+const CardList = (props) => {
+  const { drinks } = props;
+  return (
+    <div className="card-list">
+      {drinks.map((drink) => {
+        return <Card drink={drink} key={drink.idDrink} />;
+      })}
+    </div>
+  );
+};
 
-class CardList extends Component {
-  render() {
-    const { drinks } = this.props;
-    return (
-      <div className="card-list">
-        {drinks.map((drink) => {
-          return <Card  drink = {drink} key={drink.idDrink} />
-        })}
-      </div>
-    );
-  }
-}
+// class CardList extends Component {
+//   render() {
+//     const { drinks } = this.props;
+//     return (
+//       <div className="card-list">
+//         {drinks.map((drink) => {
+//           return <Card  drink = {drink} key={drink.idDrink} />
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
 export default CardList;
